20e4f3d3c46bc7838166ac6f53b0d311b80bb78b
Simon Minet <minet.simon@gmail.com>
2017-05-02 15:47:12 +0200

    chg: clean Annonce et changement des types d'annonces

--- controllers/ANNAnnonce.js
+++ controllers/ANNAnnonce.js
@@ -1,29 +1,29 @@
 /*definition de la Classe Annonce*/ /* code erreur 01*/
 function Annonce()
 {
-	var ici=this;
+	var ici = this;
 	this.id_nb; /*identifiant de l'annonce. Cle de la base de donnée*/
 	this.nom_str; /*nom de l'annonce*/
 	
 	this.photo_str; /*nomdu fichier qui contient la photo liée à l'annonce*/
-	this.description_str; /* description de l'annonce en chaine de caractere sans attribut*/
-	this.personnesMax_nb; /*nombre de personnes maximum*/
-	this.personnesMin_nb; /*nombre de personnes minimum*/
-	this.personnesInscrites_ar=[]; /*tableau de id users, liste des participants à l'annonce*/
-	this.salleDAttente_ar=[]; /*tableau de Id users, liste des participants en liste d'attente par ordre de priorité*/
-	this.personnesBannies_ar=[]; /*tableau de Id d'users; liste des participants bannis*/
-	this.lieu_nb;  /*Id clé) du Lieu auquel est attaché l'annonce. */
+	this.description_str = ""; /* description de l'annonce en chaine de caractere sans attribut*/
+	this.personnesMax_nb = 0; /*nombre de personnes maximum*/
+	this.personnesMin_nb = 0; /*nombre de personnes minimum*/
+	this.personnesInscrites_ar = []; /*tableau de id users, liste des participants à l'annonce*/
+	this.salleDAttente_ar = []; /*tableau de Id users, liste des participants en liste d'attente par ordre de priorité*/
+	this.personnesBannies_ar = []; /*tableau de Id d'users; liste des participants bannis*/
+	this.lieu_lie;  /*Id clé) du Lieu auquel est attaché l'annonce. */
 	this.dateDebut_dat; /*date  du début de l'activité de cette annonce*/
 	this.dateFinInscriptions_dat; /*Date   limite pour les inscriptions */
 	this.dateFin_dat; /*date  de la fin de l'activité*/
 	this.dateCreation_dat; /*date de creation de l'annonde. Se met automatiquement*/
 	this.gestionnaire_nb; /* id du gestionnaire */
 	
-	this.validite_bl=false; /*booleen annonce validee=true*/
-	this.annulee_bl=false; /*booleen annonce annulée Pour la recréer, il faut en recréer une, avec un Id different*/
+	this.validite_bl = false; /*booleen annonce validee=true*/
+	this.annulee_bl = false; /*booleen annonce annulée Pour la recréer, il faut en recréer une, avec un Id different*/
 	this.limiteAge_nb; /*age minimum requis pour la participation en années*/
 	this.conditionsDAdmition_st; /*chaine de caractere decrivant d'autres conditions d'admission*/
-	this.centresDInterets_ar; /*Liste des centres d'interets auquels se rapporte cette annonce.¨Pour cibler les éventuels "clients"*/
+	this.centresDInterets_ar = []; /*Liste des centres d'interets auquels se rapporte cette annonce.¨Pour cibler les éventuels "clients"*/
 	this.salleDeTchat_nb; /* Salle de tchat specifisue pour cette activité */
 
 
@@ -31,7 +31,7 @@ function Annonce()
 
 this.hydrate = function(obj)
 {
-	console.log("Annonce obj >", obj);
+	//console.log("Annonce obj >", obj);
     for (var i in obj)
     {
         this[i] = obj[i];
