6dbce40f3ae8dd0a4825cf02f6c043678fc43e27
xozeus <xozeus@gmail.com>
2017-05-02 17:20:28 +0200

    ajouts des fichiers HTML pour afficher la Classe Lieu

--- controllers/LIELieu_test.js
+++ controllers/LIELieu_test.js
@@ -4,7 +4,6 @@
  */
 
 var jsonFile =
-    [
         {
             "id_nb": 0,
             "nom_str": "Lieu1",
@@ -39,12 +38,12 @@ var jsonFile =
                         "description_str": "Qui veut jouer à la belote ?",
                         "dateDebut_str": "17-04-2017:15.00",
                         "dateFin_str": "17-04-2017:17.00",
-                        "dateCreattion_str" : "17-04-2017:17.00",
+                        "dateCreation_str" : "17-04-2017:17.00",
                         "lieu_lie": "N°5",
                         "centresInterets_ar": ["belote", "tarot", "poker", "apéro"],
                         "placesMin_nb": 4,
                         "placesMax_nb": 4,
-                        "image_Img": "./images/belote.jpg",
+                        "image_img": "./images/belote.jpg",
                         "participants_ar": [],
                         "salleAttente_ar": [],
                         "banis_ar": [],
@@ -56,12 +55,12 @@ var jsonFile =
                         "description_str": "Qui veut jouer à la belote ?",
                         "dateDebut_str": "17-04-2017:15.00",
                         "dateFin_str": "17-04-2017:17.00",
-                        "dateCreattion_str" : "17-04-2017:17.00",
+                        "dateCreation_str" : "17-04-2017:17.00",
                         "lieu_lie": "N°5",
                         "centresInterets_ar": ["belote", "tarot", "poker", "apéro"],
                         "placesMin_nb": 4,
                         "placesMax_nb": 8,
-                        "image_Img": "./images/poker.jpg",
+                        "image_img": "./images/poker.jpg",
                         "participants_ar": [],
                         "salleAttente_ar": [],
                         "banis_ar": [],
@@ -73,12 +72,12 @@ var jsonFile =
                         "description_str": "Qui veut jouer au rami avec moi ?",
                         "dateDebut_str": "17-04-2017:15.00",
                         "dateFin_str": "17-04-2017:17.00",
-                        "dateCreattion_str" : "17-04-2017:17.00",
+                        "dateCreation_str" : "17-04-2017:17.00",
                         "lieu_lie": "Café du passage",
                         "centresInterets_ar": ["belote", "tarot", "poker", "rami", "apéro"],
                         "placesMin_nb": 3,
                         "placesMax_nb": 5,
-                        "image_Img": "./images/rami.jpg",
+                        "image_img": "./images/rami.jpg",
                         "participants_ar": [],
                         "salleAttente_ar": [],
                         "banis_ar": [],
@@ -86,9 +85,22 @@ var jsonFile =
                     }
                 ]
         }
-    ];
+    ;
 
 var monLieu = new LIELieu();
 monLieu.hydrater(jsonFile);
 console.log(monLieu);
+afficherLieux();
 
+function afficherLieux()
+{
+    document.getElementById('nom_str').innerHTML = "testtttttt";
+    document.getElementById('adresse_str').innerHTML = monLieu.adresse_str;
+    document.getElementById('sousLieux_ar').innerHTML = monLieu.sousLieux_ar.toString();
+    //document.getElementById('image_img').innerHTML = monLieu.image_img;
+    document.getElementById('description_str').innerHTML = monLieu.description_str;
+    //document.getElementById('qrcode_img').innerHTML = monLieu.qrcode_img;
+    document.getElementById('latitude_nb').innerHTML = monLieu.latitude_nb.toString();
+    document.getElementById('longitude_nb').innerHTML = monLieu.longitude_nb.toString();
+    document.getElementById('annonces_ar').innerHTML = monLieu.annonces_ar.toString();
+}
