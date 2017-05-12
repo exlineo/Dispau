95e14ef043686f16227d3868580e326dc6ae8461
MORA Stephen <stenm64@gmail.com>
2017-05-12 16:57:26 +0200

    chg: demande ami in user; new: ListDemandeAMi

---
+++ groupe2/controllers/DemandeAmi.js
@@ -0,0 +1,13 @@
+function DemandeAmi(){
+     var da = this;
+    this.de_user = de_id;
+    this.a_user = a_id;
+    this.date = _date;
+    this.status = 0;
+    
+    this.sendDemande = function(ListUtilisateur){
+        if(da.a_user == true){
+
+        }
+    }
+}
--- groupe2/controllers/user.js
+++ groupe2/controllers/user.js
@@ -10,7 +10,7 @@
 
 function Profil(_pseudo_str, _email_str, _pass_str) {
     var ici = this;
-	this.id_nb ;
+	this.id_nb;
     this.pseudo_str = _pseudo_str;
     this.email_str = (_email_str && _pass_str)?_email_str : "guess"+ici.id_nb+"@dispau.com";
     this.pass_str = _pass_str || null;
@@ -50,14 +50,28 @@ function Utilisateur(_pseudo_str, _email_str, _pass_str) {
     this.abonnementsLieux_arr = [];
 
     /**
+     * Recuperer toute les informations relatif d'un utilisateur via son l'id
+     */
+    this.recupererInformationDunUtilisateur = function(idUtilisateur_nb){
+        if(ici.Utilisateur.id_nb == idUtilisateur_nb){
+            for(var r = 0; r < ici.Utilisateur.length; r++){
+                var stockInformationUser = [];
+                stockInformationUser.push(Utilisateur[r]);
+            }
+        }else{
+            return false;
+        }
+    }
+
+    /**
      * demanderAmi
      * @function
      * @param {obj} Utilisateur
      */
     this.demanderAmi = function(_Utilisateur) {
     	var utile = new DemanderAmi();
-    	utile.de_usr = ici.id_nb;
-    	utile.a_ussr = Utilisateur.id_nb;
+    	utile.de_user = ici.id_nb;
+    	utile.a_user = Utilisateur.id_nb;
     	utile.date_date = new Date.now();
     	return utile;
 	}
@@ -66,7 +80,18 @@ function Utilisateur(_pseudo_str, _email_str, _pass_str) {
      * validerDemandeAmi
      * @function
      */
-    this.validerDemandeAmi = function() {};
+    this.validerDemandeAmi = function(validation) {
+        if(ici.demandeAmi == true){
+            if(validation = true){
+                ici.amis_arr.push(ici.demanderAmi.a_user);
+                ici.demanderAmi.a_user.amis_arr.push(ici.demanderAmi.de_user); // a verifier, test!
+            }else{
+                alert('Validation refusé !');
+            }
+        }else{
+            alert('Demande d\'amis inexistante');
+        }
+    };
     
     /**
      * onNotificationRecue
