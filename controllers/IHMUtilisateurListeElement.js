3ca01a7c1d83721ba59e773000439322d8cb557c
alexDonazzan <alexandre.donazzan@gmail.com>
2017-05-19 17:15:14 +0200

    new : IHMUtilisateurListeElement

--- controllers/IHMUtilisateurListeElement.js
+++ controllers/IHMUtilisateurListeElement.js
@@ -1,3 +1,21 @@
 /**
  * Created by alexa on 19/05/2017.
  */
+
+function UtilisateurListeElement() {
+
+    utilisateurNomTxt: Element
+    utilisateurImg: Element
+    banirBtn: Element
+    function afficherUtilisateur(tableauDUsers) {
+        var listUsers = document.getElementById("utilisateurListe");
+        for (var i in tableauDUsers) {
+            var li = document.createElement("li");
+            var span = document.createElement("span");
+            var bannir = document.createElement("button");
+            var supp = document.createElement("button");
+
+
+        }
+    }
+}
