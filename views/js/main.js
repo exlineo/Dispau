9eccc7b88f1ee63a656fde33f3b2d986bbb71edb
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-05-22 19:21:16 +0200

    Create main.js
    Adds a js class made by Tahar which creates all kind of notification messages.

---
+++ views/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+
+    var test = new IHMAlert();
+
+    //test.creerAjaxAlerte('request');
+    //test.creerAjaxAlerte('success');
+    test.creerAjaxAlerte('error');
+
+    //test.creerChatAlerte('newMessage', 'monculcestdupoulet');
+    //test.creerChatAlerte('blockMessage', 'monculcestdupoulet');
+
+    //test.creerAmisAlerte('newFriend', 'monculcestdupoulet');
+    //test.creerAmisAlerte('removeFriend', 'monculcestdupoulet');
+
+});
