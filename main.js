1886b53fa598c7e31857ec5464854cc3e1963f62
alexDonazzan <alexandre.donazzan@gmail.com>
2017-06-01 16:33:18 +0200

    new : main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by alexa on 01/06/2017.
+ */
+
+var listeAnnonce = angular.module("listeAnnonce", []);
+
+listeAnnonce.controller("ListeAnnoncesCtrl", ["$http", "$scope", AnnonceListe]);
