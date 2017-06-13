7c855cad389a4d80beb01af33280234b0d66d210
xozeus <xozeus@gmail.com>
2017-06-13 16:13:21 +0200

    creation des classes vides bien nommées

--- php/lieu.php
+++ php/lieu.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ANNONCE.PHP
+ * LIEU.PHP
  *
  * Reçoit les requêtes AJAX depuis l'application AngularJS.
  * Traîte la requête HTTP.
@@ -12,20 +12,20 @@
  */
 
 /* Exemples:
- * Récupérer un annonce :
- * GET http://serveur/php/annonce.php?action=get&id=495039
+ * Récupérer un lieu :
+ * GET http://serveur/php/lieu.php?action=get&id=495039
  *
- * Récupére toutes les annonces d'un lieu :
- * GET http://serveur/php/annonce.php?action=get&lieu=49549
+ * Récupére toutes les lieus d'un lieu :
+ * GET http://serveur/php/lieu.php?action=get&lieu=49549
  *
- * Créer une annonce :
- * POST http://serveur/php/annonce.php?action=create
+ * Créer une lieu :
+ * POST http://serveur/php/lieu.php?action=create
  *
- * Modifier une annonce :
- * POST http://serveur/php/annonce.php?action=update&id=495439
+ * Modifier une lieu :
+ * POST http://serveur/php/lieu.php?action=update&id=495439
  *
- * Supprimer une annonce :
- * GET http://serveur/php/annonce.php?action=delete&id=43953429
+ * Supprimer une lieu :
+ * GET http://serveur/php/lieu.php?action=delete&id=43953429
  */
 
 /* Méthodes à coder :
