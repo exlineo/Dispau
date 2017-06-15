fb83322329c530cdb7cb400109710d2359c54227
xozeus <xozeus@gmail.com>
2017-06-15 17:01:22 +0200

    Classes Lieu et SousLieu mises à jour et commentées

--- models/LIESousLieu.js
+++ models/LIESousLieu.js
@@ -5,7 +5,35 @@
 
 function LIESousLieu()
 {
-    // properties
 
-    // methods
+    /////// Propriétés ///////
+
+
+    /**
+     * Identifiant du sous-lieu
+     * @type {number}
+     * @default 0
+     */
+    this.id_nb = 0;
+
+    /**
+     * Nom du lieu
+     * @type {string}
+     * @default ''
+     */
+    this.nom_str = '';
+
+    /**
+     * Identifiant du lieu parent
+     * @type {number}
+     * @default 0
+     */
+    this.lieuParent_nb = 0;
+
+    /**
+     * Identifiant du lieu principal
+     * @type {number}
+     * @default 0
+     */
+    this.lieuPrincipal_nb = 0;
 }
