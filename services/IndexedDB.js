01412d1a76329cdba0fba0ba7153d46509d45a94
xozeus <xozeus@gmail.com>
2017-06-19 17:25:23 +0200

    chg : ajout colonne 'modifications' dans toutes les tables new : ajout de app.js qui servira a gérer toute l'application Angular

--- services/IndexedDB.js
+++ services/IndexedDB.js
@@ -17,19 +17,21 @@ function IndexedDB() {
     var databaseDeclarations = {
 
         0: {
-            "annonce":    "++id, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb",
+            "annonce":    "++id, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb, modifications",
 
-            "centreDInteret":  "++id, nom_str",
+            "centreDInteret":  "++id, nom_str, modifications",
 
-            "lieu": "++id, demandeur_nb, cible_nb, dateDemande_dat",
+            "lieu": "++id, demandeur_nb, cible_nb, dateDemande_dat, modifications",
 
-            "demandeAmi": "++id, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb",
+            "demandeAmi": "++id, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb, modifications",
 
-            "message": "++id, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb",
+            "message": "++id, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb, modifications",
 
-            "sousLieu": "++id, nom_str, lieuParent_nb, lieuPrincipal_nb, sousLieu_ar",
+            "sousLieu": "++id, nom_str, lieuParent_nb, lieuPrincipal_nb, sousLieu_ar, modifications",
 
-            "utilisateur": "++id, pseudo_str, prenom_str, pass_str, email_str, description_str, nom_str, dateInscription_date, abonnementLieu_ar, notificationRecue_ar, demandeAmi_ar, ami_ar, annonceParticipee_ar, grade_nb, photoProfil_str"
+            "utilisateur": "++id, pseudo_str, prenom_str, pass_str, email_str, description_str, nom_str, dateInscription_date, abonnementLieu_ar, notificationRecue_ar, demandeAmi_ar, ami_ar, annonceParticipee_ar, grade_nb, photoProfil_str, modifications",
+
+            "requests": "++id, method, url, body, timestamp"
 
         }
     };
