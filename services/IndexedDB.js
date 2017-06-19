7063f6a1c58c0fc93bc639e0c08bd2f95ffe4cd8
xozeus <xozeus@gmail.com>
2017-06-19 14:48:10 +0200

    new : ajout du fichier Dexie.js qui permettra la connexion à la BDD new : mise a jour de la base de données (schema) dans IndexedDB.js

--- services/IndexedDB.js
+++ services/IndexedDB.js
@@ -4,21 +4,33 @@
  */
 
 /**
- * Service pour le wrapper IndexedDB Dexie.js
- * @return {Dexie}
+ * Service pour le wrapper IndexedDB.js
+ * @return {IndexedDB}
  */
 function IndexedDB() {
 
     /**
      * Liste des stores
-     * @type {{1: {photos: string}, 2: {photos: string, requests: string}, 3: {photos: string, requests: string}, 4: {photos: string, requests: string, testcases: string}, 5: {photos: string, requests: string, testcases: string}, 6: {photos: string, requests: string, testcases: string}, 7: {photos: string, requests: string, testcases: string}}}
+     * Represente la base de données au format Dexie, avec les tables
+     * ATTENTION : il est possible d'utiliser l'* pour caractériser un tableau de clé (utile ???)
      */
     var databaseDeclarations = {
 
-        {
-            "photos":    "key, value, modifications",
-            "requests":  "++id, method, url, body, timestamp",
-            "testcases": "&id, nom, description, numero, date, modifications"
+        0: {
+            "annonce":    "++id, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb",
+
+            "centreDInteret":  "++id, nom_str",
+
+            "lieu": "++id, demandeur_nb, cible_nb, dateDemande_dat",
+
+            "demandeAmi": "++id, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb",
+
+            "message": "++id, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb",
+
+            "sousLieu": "++id, nom_str, lieuParent_nb, lieuPrincipal_nb, sousLieu_ar",
+
+            "utilisateur": "++id, pseudo_str, prenom_str, pass_str, email_str, description_str, nom_str, dateInscription_date, abonnementLieu_ar, notificationRecue_ar, demandeAmi_ar, ami_ar, annonceParticipee_ar, grade_nb, photoProfil_str"
+
         }
     };
 
