1279f5f24ba5f9064e236afd8a15811693109fad
Simon Minet <minet.simon@gmail.com>
2017-06-22 14:34:15 +0200

    chg : indexedDB.js : modification des 'id_nb'

--- services/IndexedDB.js
+++ services/IndexedDB.js
@@ -19,19 +19,19 @@ function IndexedDB() {
         1: {
             "annonce":    "++id, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb, modifications",
 
-            "centreDInteret":  "++id, nom_str, modifications",
+            "centreDInteret":  "++id_nb, nom_str, modifications",
 
-            "lieu": "++id, demandeur_nb, cible_nb, dateDemande_dat, modifications",
+            "demandeAmi": "++id_nb, demandeur_nb, cible_nb, dateDemande_dat, modifications",
 
             "demandeAmi": "++id, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb, modifications",
 
-            "message": "++id, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb, modifications",
+            "message": "++id_nb, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb, modifications",
 
-            "sousLieu": "++id, nom_str, lieuParent_nb, lieuPrincipal_nb, sousLieu_ar, modifications",
+            "sousLieu": "++id_nb, nom_str, lieuParent_nb, lieuPrincipal_nb, sousLieu_ar, modifications",
 
-            "utilisateur": "++id, pseudo_str, prenom_str, pass_str, email_str, description_str, nom_str, dateInscription_date, abonnementLieu_ar, notificationRecue_ar, demandeAmi_ar, ami_ar, annonceParticipee_ar, grade_nb, photoProfil_str, modifications",
+            "utilisateur": "++id_nb, pseudo_str, prenom_str, pass_str, email_str, description_str, nom_str, dateInscription_date, abonnementLieu_ar, notificationRecue_ar, demandeAmi_ar, ami_ar, annonceParticipee_ar, grade_nb, photoProfil_str, modifications",
 
-            "request": "++id, method, url, body, timestamp"
+            "request": "++id_nb, method, url, body, timestamp"
 
         }
     };
