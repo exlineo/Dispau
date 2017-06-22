68556a4859f5d94e9f0ef73b96b8d594fe02a055
Simon Minet <minet.simon@gmail.com>
2017-06-22 14:35:31 +0200

    Auto stash before cherrypick of "chg : indexedDB.js : modification des 'id_nb'"

--- services/IndexedDB.js
+++ services/IndexedDB.js
@@ -17,13 +17,13 @@ function IndexedDB() {
     var databaseDeclarations = {
 
         1: {
-            "annonce":    "++id, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb, modifications",
+            "annonce":    "++id_nb, nom_str, image_str, dateCreation_date, dateFinInscription_date, dateFin_date, dateDebut_date, idLieu_nb, placeMin_nb, placeMax_nb, idGestionnaire_nb, idChat_nb, salleDAttente_ar, participant_ar, centreInteret_ar, validite_nb, modifications",
 
             "centreDInteret":  "++id_nb, nom_str, modifications",
 
             "demandeAmi": "++id_nb, demandeur_nb, cible_nb, dateDemande_dat, modifications",
 
-            "demandeAmi": "++id, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb, modifications",
+            "lieu": "++id_nb, nom_str, idChat_nb, longitude_nb, latitude_nb, annonce_ar, sousLieu_ar, utilisateur_ar, adresse_str, description_str, image_str, idAdmin_nb, modifications",
 
             "message": "++id_nb, idChat_nb, date_date, banni_nb, contenu_str, idExpediteur_nb, modifications",
 
