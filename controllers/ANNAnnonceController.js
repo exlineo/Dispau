ec5412a6b1fa9327ccc4624e9e763204b07aa364
Simon Minet <minet.simon@gmail.com>
2017-06-23 11:07:17 +0200

    chg: ajout de commentaires

--- controllers/ANNAnnonceController.js
+++ controllers/ANNAnnonceController.js
@@ -1,13 +1,19 @@
 /**
+ * Classe qui permet d'afficher un lieu par rapport à un ID
+ * @param DBManager     Gestion de la BD
+ * @param $routeParams  Gestion des paramètres dans les routes
  * @constructor
  */
 function ANNAnnonceController (DBManager, $routeParams) {
-    // Properties
+    //Récupère l'id dans la route
     this.id = $routeParams.idAnnonce
 
     var vm = this;
     this.model = null;
 
+    /**
+     * Permet de récupérer une annonce par rapport à son ID
+     */
     DBManager.get('ANNAnnonce', vm.id)
         .then(function (annonce) {
             console.log(annonce);
