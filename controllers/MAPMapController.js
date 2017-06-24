c7cc1a67a6405efb4e305f289a848e425b194093
alexDonazzan <alexandre.donazzan@gmail.com>
2017-06-24 16:01:10 +0200

    chg : Réorganisation du controller de la carte avant l'utilisation du DBmanager pour les markers.

--- controllers/MAPMapController.js
+++ controllers/MAPMapController.js
@@ -1,69 +1,133 @@
-/**
- * Created by alexa on 15/06/2017.
- */
-
-function MAPMap(uiGmapGoogleMapApi, $scope) {
-
-    // var uiGmapGoogleMapApi = les choses récupérées depuis l'API google
 
 
-    //Initialisation des variables
-    $scope.map = {
-        center: {
-            latitude: 43.2936835, //Position initial de la carte
-            longitude: -0.3756524
+//style de la map
+
+var styleMap = [{
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [{
+        "saturation": "-100"
+    }]
+},
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": -100
         },
-        zoom: 11 // de 0 à 19, 0 étant la valeur de zoom la plus forte
-    };
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }, {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": -100
+        },
+            {
+                "lightness": "50"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }, {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": "-100"
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [{
+            "lightness": "30"
+        }]
+    }, {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [{
+            "lightness": "40"
+        }]
+    }, {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "hue": "#ffff00"
+        }, {
+            "lightness": -25
+        }, {
+            "saturation": -97
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [{
+            "lightness": -25
+        }, {
+            "saturation": -100
+        }]
+    }
+];
 
-    $scope.markers = [{
-        coord: [
-            43.2936835, //Coordonnées où placer le point
-            -0.3756524
-        ],
-        email: "netapsys@netapsys.fr", //Propriété métier, pour les afficher à l'utilisateur lorsqu'il sélectionne le point par exemple
-        icon: "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi.png", //Icone personnalisée
-        id: 412
-    },{
-        coord: [
-            46.5132,
-            0.1033
-        ],
-        email: "netapsys@netapsys.fr",
-        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png", //Icone personnalisée
-        id: 413
-    }];
 
-    $scope.clickMarker = function(marker) {
-        alert(marker.email); //Affichera l'email du point sur lequel on a cliqué
-    };
+function afficheMap(NgMap) {
 
-    uiGmapGoogleMapApi.then(function(maps) {
-        var richmarkerScript = document.createElement('script');
-        richmarkerScript.type = 'application/javascript';
-        richmarkerScript.src = 'bower_components/js-rich-marker/src/richmarker-compiled.js';
+    var vmm = this;
 
-        var markerCluster = document.createElement('script');
-        markerCluster.type = 'application/javascript';
-        markerCluster.src = 'bower_components/markerclustererplus/dist/markerclusterer.min.js';
+    NgMap.getMap('dispauCarte').then(function(map) {
+        vmm.map = map;
+        // ajout du style de la map
+        vmm.map.setOptions({styles: styleMap});
 
-        var markerWithLabelScript = document.createElement('script');
-        markerWithLabelScript.type = 'application/javascript';
-        markerWithLabelScript.src = 'bower_components/google-maps-utility-library-v3-markerwithlabel/dist/markerwithlabel.js';
-
-        var infoBoxScript = document.createElement('script');
-        infoBoxScript.type = 'application/javascript';
-        infoBoxScript.src = 'bower_components/google-maps-utility-library-v3-infobox/dist/infobox.js';
-
-        var keydragzoomScript = document.createElement('script');
-        keydragzoomScript.type = 'application/javascript';
-        keydragzoomScript.src = 'bower_components/google-maps-utility-library-v3-keydragzoom/dist/keydragzoom.js';
-
-        document.body.appendChild(richmarkerScript);
-        document.body.appendChild(markerCluster);
-        document.body.appendChild(markerWithLabelScript);
-        document.body.appendChild(infoBoxScript);
-        document.body.appendChild(keydragzoomScript);
+        vmm.map.onClick = function() {
+            alert('Carte cliquée');
+        }
     });
+
+    vmm.positions1 = [
+        { pos: [40.11, -0.21], name: 1 }, { pos: [40.22, -0.10], name: 2 },
+        { pos: [40.33, -0.99], name: 3 }, { pos: [40.44, -0.88], name: 4 },
+        { pos: [40.55, -0.77], name: 5 }, { pos: [40.66, -0.66], name: 6 }
+    ];
+
+    vmm.positions2 = [
+        { pos: [40.71, -0.21], name: 1 }, { pos: [41.72, -0.20], name: 2 },
+        { pos: [40.73, -0.19], name: 3 }, { pos: [40.74, -0.18], name: 4 },
+        { pos: [40.75, -0.17], name: 5 }, { pos: [43.76, -0.16], name: 6 }
+    ];
+
+    vmm.setPositions = function(pos) {
+        vmm.positions = angular.copy(pos);
+    };
+
+    vmm.setPositions(vmm.positions2);
+    vmm.currentIndex = 0;
+    vmm.selectNextCustomMarker = function() {
+    }
 
 }
