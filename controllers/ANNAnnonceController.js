bf35887f2d958b3e00381f408c2285d1aae078c1
Gael PHILIPPE <contact@gael-philippe.fr>
2017-06-25 17:50:27 +0200

    ajout du test d'insertion d'une annonce dans annonce controller. todo: enlever ça.

--- controllers/ANNAnnonceController.js
+++ controllers/ANNAnnonceController.js
@@ -11,14 +11,26 @@ function ANNAnnonceController (DBManager, $routeParams) {
     var dbManager = DBManager('ANNAnnonce');
 
     dbManager.all()
-        .where('id_nb').equals(5)
+        .where('id_nb').equals(1)
         .limit(5)
         .then(function (annonces) {
             console.log(annonces[0]);
             vm.model = annonces[0];
+
+            var annonceToSave = annonces[0];
+            annonceToSave.id_nb = -1;
+            annonceToSave.modifierNom('nom à enregistrer');
+
+            dbManager.save(annonceToSave)
+                .then(function (annonceSaved) {
+                    vm.model = annonceSaved;
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
         })
         .catch(function (error) {
             console.log(error);
             console.log("ERROR")
-        })
+        });
 }
