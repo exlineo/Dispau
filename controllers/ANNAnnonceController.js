9643df4d1d58b5cb60a9b1c10f0b1c788c5eaa18
Gael PHILIPPE <contact@gael-philippe.fr>
2017-06-25 18:28:11 +0200

    ajout de commentaire de ANNAnnonceController

--- controllers/ANNAnnonceController.js
+++ controllers/ANNAnnonceController.js
@@ -1,36 +1,79 @@
 /**
+ * Controlleur pour une annonce
+ * @param {dbManagerFactory} DBManager Fabrique de gestionnaire d'entités
+ * @param {$routeParams} $routeParams Service de paramètres de route
  * @constructor
  */
 function ANNAnnonceController (DBManager, $routeParams) {
-    // Properties
+    /**
+     * Réference locale
+     * @type {ANNAnnonceController}
+     * @private
+     */
+    var vm = this;
+
+    /**
+     * Gestionnaire d'entités
+     * @type {DBManager}
+     * @private
+     */
+    var annonceManager = DBManager('ANNAnnonce');
+
+    /**
+     * Identifiant de l'annonce
+     * @type {number}
+     */
     this.id = $routeParams.id;
 
-    var vm = this;
+    /**
+     * L'annonce contrôlée
+     * @type {ANNAnnonce|null}
+     */
     this.model = null;
 
-    var dbManager = DBManager('ANNAnnonce');
-
-    dbManager.all()
-        .where('id_nb').equals(1)
-        .limit(5)
-        .then(function (annonces) {
-            console.log(annonces[0]);
-            vm.model = annonces[0];
-
-            var annonceToSave = annonces[0];
-            annonceToSave.id_nb = -1;
-            annonceToSave.modifierNom('nom à enregistrer');
-
-            dbManager.save(annonceToSave)
-                .then(function (annonceSaved) {
-                    vm.model = annonceSaved;
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
+    // Récupération de l'annonce
+    annonceManager.get(vm.id)
+        .then(function (annonce) {
+            console.log(annonce);
+            vm.model = annonce;
         })
         .catch(function (error) {
             console.log(error);
             console.log("ERROR")
         });
+
+    /**
+     * Enregistre une annonce, la met à jour si elle existe
+     * @param {object} donnees
+     */
+    this.enregistrerAnnonce = function (donnees) {
+        // Hydratation de l'instance à envoyer
+        var annonce = new ANNAnnonce();
+        annonce.hydrater(donnees);
+
+        // Envoi de la requête d'insertion
+        annonceManager.save(annonce)
+            .then(function (annonceEnregistree) {
+                // TODO: traîtement à l'ajout
+            })
+            .catch(function (error) {
+                // TODO: traîter l'erreur
+            });
+    };
+
+    /**
+     * Supprime une annocne
+     * @param {number} annonceId
+     */
+    this.supprimerAnnonce = function (annonceId) {
+        if (confirm('Voulez-vous supprimer cette annonce ?')) {
+            annonceManager.delete(annonceId)
+                .then(function (annonceId) {
+
+                })
+                .catch(function (error) {
+
+                });
+        }
+    };
 }
