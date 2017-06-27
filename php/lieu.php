0fe1fba3b13f18a078aac8437f2b00bb7c10ec9a
xozeus <xozeus@gmail.com>
2017-06-27 17:09:50 +0200

    new: ajout des fonctions insert, select, delete

--- php/lieu.php
+++ php/lieu.php
@@ -18,13 +18,13 @@
  * Récupére toutes les lieus d'un lieu :
  * GET http://serveur/php/lieu.php?action=get&lieu=49549
  *
- * Créer une lieu :
+ * Créer un lieu :
  * POST http://serveur/php/lieu.php?action=create
  *
- * Modifier une lieu :
+ * Modifier un lieu :
  * POST http://serveur/php/lieu.php?action=update&id=495439
  *
- * Supprimer une lieu :
+ * Supprimer un lieu :
  * GET http://serveur/php/lieu.php?action=delete&id=43953429
  */
 
@@ -35,48 +35,192 @@
  *  - delete()
  */
 
+// Connexion BDD
 require_once 'connectDB.php';
 
 // Exemple pour récupérer le PDO
 $db = db();
 
-getLieu($db);
+
+/////////// APPELS DES METHODES //////////
 
 
-function getLieu($db)
+//getLieu($db);
+
+
+/////////// DECLARATION DES METHODES ////////////////
+
+
+
+
+/**
+ * Permet de récupérer l'id de l'annonce s'il y en a un
+ */
+
+if (isset($_GET['id']))
+    $id = $_GET['id'];
+else
+    $id = null;
+
+if (isset($_GET['where']))
+    $where = $_GET['where'];
+else
+    $where = false;
+
+switch ($_GET['action'])
+{
+    case "get" :
+        echo selectLieu($db, $id);
+        break;
+
+    case "create" :
+        $data = json_decode(file_get_contents("php://input"), true);
+        echo insertLieu($db, $data);
+        break;
+
+    case "update" :
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (isset($_GET['id'])) {
+            echo updateLieu($db, $id, $data);
+        }
+        break;
+
+    case "delete" :
+        deleteLieu($db, $id);
+        break;
+}
+
+
+/**
+ * Fonction SELECT
+ */
+
+
+function selectLieu($db, $id)
 {
     try
     {
-        $stmt = $db->prepare("SELECT * FROM `lieu` WHERE `id_nb`= 1");
-        $stmt->execute();
-
-        while ($data = $stmt->fetch(PDO::FETCH_ASSOC))
+        // SELECT d'un seul lieu
+        if (isset($id))
         {
-            header('Content-Type: application/json;charset=utf8;');
-            echo json_encode($data);
+            $stmt = $db->prepare("SELECT * FROM `lieu` WHERE `id_nb`= :id_nb");
+            $stmt->execute(array('id_nb' => $id));
+
+            $data = $stmt->fetch(PDO::FETCH_ASSOC));
         }
+
+        // SELECT de tous les lieux
+        else
+        {
+            $stmt = $db->prepare("SELECT * FROM `lieu`);
+            $stmt->execute();
+
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC));
+        }
+
+        header('Content-Type: application/json;charset=utf8;');
+        return json_encode($data);
     }
+
     catch(Exception $e)
     {
         exit('<b>Catched exception at line '. $e->getLine() .' (code : '. $e->getCode() .') :</b> '. $e->getMessage());
     }
 }
 
-function getLieux($db)
-{
-    try
-    {
-        $stmt = $db->prepare("SELECT * FROM `lieu`");
-        $stmt->execute();
 
-        while ($data = $stmt->fetch(PDO::FETCH_ASSOC))
+function insertAnnonce($db, $data)
+{
+    // Il ne faut pas d'id, c'est le SGBD qui nous la donne
+    // On supprime la clé pour ne pas insérer de fausses valeurs
+    unset($data['id_nb']);
+
+    // Ce tableau devra servir à gérer les association "one to many" et "many to one"
+    // TODO: taiter les associations
+
+    $assocs = [];
+
+    // Pour éviter les erreurs avec les associations
+    // on supprime les champs avec des associations (qui finissent pas '_ar')
+    foreach ($data as $key => $value) {
+        // Les trois lignes qui suivent servent à trouver les champs
+        // qui finissent pas '_ar'
+
+        $l = strlen('_ar');
+        $substr = substr($key, -$l);
+        if ($substr === '_ar')
         {
-            header('Content-Type: application/json;charset=utf8;');
-            echo json_encode($data);
+            // Suppression des clés qui poseront promblème
+            $assoc[$key] = $data[$key];
+            unset($data[$key]);
         }
+    };
+    // On récupère la liste des champs à mettre à jour
+    $champs_ar = array_keys($data);
+    // Création de la chaîne représentant la liste des champs à insérer
+    $champs_str = implode(', ', $champs_ar);
+    // Création des la liste des paramètres à passer à PDO::prepare
+    $placeholders_ar = array_map(function ($champ) {
+        return ":$champ";
+    }, $champs_ar);
+    // Création de la chaîne avec les paramètres
+    $placeholders_str = implode(', ', $placeholders_ar);
+
+    // Préparation de la requête d'insertion
+    $req = $db->prepare("INSERT INTO `lieu` ($champs_str) VALUES ($placeholders_str)");
+
+    // Requête de récupération du dernier id inséré
+    $lastInsertIdReq = $db->prepare("SELECT LAST_INSERT_ID();");
+
+    // Variable pour récupérer le dernier id inséré
+    // On lui donne un fausse valeur
+    $id_nb = -1;
+
+    // On commence la transaction SQL
+    try {
+        $db->beginTransaction();
+        // On exécute la requête, on envoie l'erreur si elle echoue
+        // TODO: Améliorer la gestion d'erreur
+        if (!$req->execute($data)) {
+            $error = $req->errorInfo();
+            echo json_encode($error);
+        }
+        // Récupération du dernier Id inséré
+        $lastInsertIdReq->execute();
+        $id_nb = $lastInsertIdReq->fetch();
+        // La transaction s'est bien passée, on commit
+        $db->commit();
+        // Ajout de l'identifiant dans les données à renvoyer
+        $data['id_nb'] = $id_nb[0];
+        // On remets les association to-many
+        // TODO: Traîer les associations
+        foreach ($assocs as $key => $value) {
+            $data[$key] = $value;
+        }
+        // Renvoie du résultat
+        return json_encode($data);
+    } catch (Exception $e) {
+        $db->rollback();
+        // TODO: Améliorer la gestion des erreurs
+        return json_encode([ 'message' => 'Erreur lors de l\'insertion d\'une annonce']);
     }
-    catch(Exception $e)
-    {
-        exit('<b>Catched exception at line '. $e->getLine() .' (code : '. $e->getCode() .') :</b> '. $e->getMessage());
+}
+
+
+
+function updateLieu($db, $id, $data)
+{
+
+}
+
+
+function deleteLieu($db, $id){
+    try {
+        $req = $db->prepare("DELETE FROM `lieu` WHERE id_nb = :id_nb");
+        $req->execute(array(
+            'id_nb' => $id
+        ));
+    } catch (Exception $e) {
+        echo 'ERROR: ' . $e->getMessage();
     }
 }
