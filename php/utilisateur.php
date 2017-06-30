ea2d5a5c1537bf58673e48d03f97c0b153e0bb48
Francois Labastie <flabastie@hotmail.com>
2017-06-30 11:22:47 +0200

    add: fichier utilisateur.php

--- php/utilisateur.php
+++ php/utilisateur.php
@@ -40,3 +40,83 @@ require_once 'connectDB.php';
 
 // Exemple pour récupérer le PDO
 $db = db();
+
+/**
+ * Permet de récupérer l'action
+ */
+$action = $_GET['action'];
+/**
+ * Permet de récupérer l'id de l'utilisateur
+ */
+if (isset($_GET['id']))
+    $id = $_GET['id'];
+else
+    $id = null;
+if (isset($_GET['where']))
+    $where = $_GET['where'];
+else
+    $where = false;
+
+switch ($action) {
+    case "get" :
+        if(!$id){
+            header('Content-Type: application/json;charset=utf8;');
+            echo getUtilisateurs($db);
+        }
+        else if (!$where) {
+            header('Content-Type: application/json;charset=utf8;');
+            echo getUtilisateur($db, $id);
+        } 
+        else {
+            header('Content-Type: application/json;charset=utf8;');
+            echo getUtilisateur($db, $id, $where);
+        }
+        break;
+    case "create" :
+        $data = json_decode(file_get_contents("php://input"), true);
+        echo insertUtilisateur($db, $data);
+        break;
+    case "update" :
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (isset($_GET['id'])) {
+            echo updateUtilisateur($db, $id, $data);
+        }
+        break;
+    case "delete" :
+        echo deleteUtilisateur($id);
+        break;
+}
+
+function getUtilisateurs($db)
+{
+    try
+    {
+        $stmt = $db->prepare("SELECT * FROM `utilisateur`");
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        header('Content-Type: application/json;charset=utf8;');
+        return json_encode($data);
+    }
+    catch(Exception $e)
+    {
+        exit('<b>Catched exception at line '. $e->getLine() .' (code : '. $e->getCode() .') :</b> '. $e->getMessage());
+    }
+}
+
+function getUtilisateur($db)
+{
+    try
+    {
+        $stmt = $db->prepare("SELECT * FROM `utilisateur` WHERE `id_nb`= :id");
+        $stmt->execute(['id' => $_GET['id']]);
+        while ($data = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            header('Content-Type: application/json;charset=utf8;');
+            return json_encode($data);
+        }   
+    }
+    catch(Exception $e)
+    {
+        exit('<b>Catched exception at line '. $e->getLine() .' (code : '. $e->getCode() .') :</b> '. $e->getMessage());
+    }
+}
